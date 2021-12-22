ee341908a1c544efc28e2dc6ed72d412a0715ec4
thr3eisl4nd <thre3islands@gmail.com>
2021-12-22 17:07:32 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,9 +3,23 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.create(prototype_params) # formメソッドで使用する変数
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+   end
   end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
 
 end
