b9de5e1dbcb375b40635c90d19578e43ac0742b0
thr3eisl4nd <thre3islands@gmail.com>
2021-12-23 16:49:31 +0900

    プロトタイプ情報の編集機能/削除を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,26 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+       redirect_to prototypes_path
+    end
+  end
+
   private
 
   def prototype_params
