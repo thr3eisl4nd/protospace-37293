9cae8ddd47ff29a468dfdabebadcdf32c3bb7203
thr3eisl4nd <thre3islands@gmail.com>
2021-12-24 12:48:28 +0900

    ログインしていないユーザーのページ遷移を制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,6 +24,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user.id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
